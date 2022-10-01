d6b0db0e52f90cb2ec28b6443350f906eef62af2
mstrbstrd <shae@mstrbstrd.com>
2022-09-30 17:10:40 -0700

    adding user context, clickstream..

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,15 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { Outlet, Link } from "react-router-dom";
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { UserContext } from '../../context/user.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.css';
 
 const Navigation = () => {
+    const { currentUser } = useContext(UserContext);
+
     return (
       <Fragment>
         <div className='navigation'>
@@ -14,11 +18,16 @@ const Navigation = () => {
             </Link>
             <div className='nav-links-container'>
                 <Link className='nav-link' to='/shop'>
-                        SHOP
+                        Shop
                 </Link>
-                <Link className='nav-link' to='/auth'>
-                        SIGN IN
-                </Link>
+                {currentUser ? (
+                    <span className='nav-link' onClick={signOutUser}>
+                      Sign Out
+                    </span>
+                    ) : ( <Link className='nav-link' to='/auth'>
+                      Sign In
+                    </Link>
+                )}
             </div>
         </div>
         <Outlet />
