28a3ce2a41d03f128e427b82a8f985a88b21cfd9
mstrbstrd <shae@mstrbstrd.com>
2022-10-04 15:25:49 -0700

    fleshed out indivdual category pages as well as links to them..

---
+++ src/context/categories.context.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from "react";
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+
+// import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+// import SHOP_DATA from '../shop-data.js';
+
+export const CategoriesContext = createContext({
+    categoriesMap: [],
+
+});
+
+export const CategoriesProvider = ({children}) => { 
+    const [categoriesMap, setCategoriesMap] = useState({});
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+            setCategoriesMap(categoryMap);
+        }
+        getCategoriesMap();
+    }, []);
+
+    // vvvvv everytime that is ran it will try to set new values in the database
+
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', SHOP_DATA);
+    // })
+
+    const value = { categoriesMap };
+
+    return (
+        <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+    );
+};
